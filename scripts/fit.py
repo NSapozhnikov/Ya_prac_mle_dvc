7c8031bb226a41dd9b1dab4fdff207c7734774e2
Nikita <nikita.sapozhnikov1@gmail.com>
2025-05-16 17:04:52 +0000

    Change CatBoost to LogisticRegression

--- scripts/fit.py
+++ scripts/fit.py
@@ -4,6 +4,7 @@ from sklearn.pipeline import Pipeline
 from category_encoders import CatBoostEncoder
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from catboost import CatBoostClassifier
+from sklearn.linear_model import LogisticRegression
 import yaml
 import os
 import joblib
@@ -23,15 +24,16 @@ def fit_model():
 
     preprocessor = ColumnTransformer(
         [
-            ('binary', OneHotEncoder(drop='if_binary'), binary_cat_features.columns.tolist()),
-            ('cat', CatBoostEncoder(return_df=False), other_cat_features.columns.tolist()),
+            ('binary', OneHotEncoder(drop=params['one_hot_drop']), binary_cat_features.columns.tolist()),
+            ('cat', OneHotEncoder(drop=params['one_hot_drop']), other_cat_features.columns.tolist()),
             ('num', StandardScaler(), num_features.columns.tolist())
         ],
         remainder='drop',
         verbose_feature_names_out=False
     )
 
-    model = CatBoostClassifier(auto_class_weights='Balanced')
+    # model = CatBoostClassifier(auto_class_weights=params['auto_class_weights'])
+    model = LogisticRegression(C=params['lr_C'], penalty=params['lr_penalty'])
 
     pipeline = Pipeline(
         [
